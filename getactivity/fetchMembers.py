434aecb8e68363ae42fa086fffe53121fa355d2b
Toffrox <jeffrey.yflu@gmail.com>
2022-08-17 20:24:48 -0400

    General changes

--- getactivity/fetchMembers.py
+++ getactivity/fetchMembers.py
@@ -16,72 +16,76 @@ async def get_alliance_json():
     return response.text
 
 async def fetchMembers():
-    while True:
-        now = datetime.now()
-        currentTime = now.strftime("%d-%m-%Y %H:%M:%S")
-        print(currentTime + ": Running...")
-        
-        memberList = {'DiscordID':[], 'Date Founded':[]}
+    
+    now = datetime.now()
+    currentTime = now.strftime("%d-%m-%Y %H:%M:%S")
+    print(currentTime + ": Running...")
+    
+    memberList = {'DiscordID':[], 'Date Founded':[]}
 
-        data = await get_alliance_json()
+    data = await get_alliance_json()
 
-        try:
-            alliance = json.loads(data, strict=False)
-            if (alliance["success"]):
-                members = alliance["member_id_list"]
-                ids = getSpreadsheet()
+    alliance = json.loads(data, strict=False)
+    if (alliance["success"]):
+        members = alliance["member_id_list"]
+        ids = getSpreadsheet()
 
-                for i in members:
-                    memberLog = {'P&W Active':[], 'Discord Messages':[]}
+        for i in members:
+            memberLog = {'P&W Active':[], 'Discord Messages':[]}
+            print("Getting nation info for " + str(i))
+            nation = getNation(i)
 
-                    nation = getNation(i)
+            if (nation["success"]):
+                #append age to the all members list
+                print("Date founded: " + str(nation["founded"]))
+                memberList['Date Founded'].append(nation["founded"])
 
-                    if (nation["success"]):
-                        #append to the all members list
-                        memberList['DiscordID'].append(0)
-                        memberList['Date Founded'].append(nation["founded"])
+                #append pnw activity to individual member dataframe
+                print("Minutes since last P&W login: " + str(nation["minutessinceactive"]))
+                memberLog['P&W Active'].append(nation["minutessinceactive"])
+                
+                print("Getting Discord info...")
+                try:
+                    #append discord ID to all members list
+                    print("Discord ID: " + str(ids.loc[i].DiscordID))
+                    memberList['DiscordID'].append(ids.loc[i].DiscordID)
 
-                        #append pnw activity to individual member dataframe
-                        memberLog['P&W Active'].append(nation["minutessinceactive"])
-                        
-                        try:
-                            #get discord id from nation id, then count messages
-                            messageCount = await getDiscord(ids.loc[i].DiscordID)
-                            #append discord activity to individual member dataframe
-                            memberLog['Discord Messages'].append(messageCount)
-                        except KeyError:
-                            memberLog['Discord Messages'].append(0)
+                    #get discord id from nation id, then count messages
+                    messageCount = await getDiscord(ids.loc[i].DiscordID)
+                    print("Messages in Discord in the last 2 hours: " + str(messageCount))
+                    #append discord activity to individual member dataframe
+                    memberLog['Discord Messages'].append(messageCount)
+                except KeyError:
+                    print("No ID found")
+                    memberList['DiscordID'].append(0)
+                memberLog['Discord Messages'].append(0)
 
-                        #create individual member dataframe
-                        memberDF = pd.DataFrame(memberLog, index = [currentTime])
+                #create individual member dataframe
+                memberDF = pd.DataFrame(memberLog, index = [currentTime])
 
-                        #if individual member activity csv exists, then append, if not, create
-                        memberFile = os.path.abspath('./memberactivity/' + str(i) + '.csv')
-                        if (os.path.isfile(memberFile)):
-                            memberDF.to_csv(memberFile, header=False, mode='a')
-                        else:
-                            memberDF.to_csv(memberFile, header=['P&W Active', 'Discord Messages'])
-                    else:
-                        print(nation["error"])
-
-                #create dataframe for list of all members
-                membersDF = pd.DataFrame(memberList, members)
-                membersDF.to_csv(os.path.abspath('./members.csv'), header=['DiscordID', 'Date Founded'])
-
+                #if individual member activity csv exists, then append, if not, create
+                memberFile = os.path.abspath('./memberactivity/' + str(i) + '.csv')
+                if (os.path.isfile(memberFile)):
+                    memberDF.to_csv(memberFile, header=False, mode='a')
+                else:
+                    memberDF.to_csv(memberFile, header=['P&W Active', 'Discord Messages'])
             else:
-                print(alliance["error"])
-        except ValueError:
-            print ("Decoding JSON Failed")
-        except AttributeError:
-            print ("Parsing Failed")
+                print(nation["error"])
 
-        await asyncio.sleep(7200)
+        #create dataframe for list of all members
+        membersDF = pd.DataFrame(memberList, members)
+        membersDF.to_csv(os.path.abspath('./members.csv'), header=['DiscordID', 'Date Founded'])
+
+    else:
+        print(alliance["error"])
+
 
 #event loop
 loop = asyncio.get_event_loop()
 try:
     asyncio.ensure_future(fetchMembers())
     loop.run_forever()
+    loop.run_until_complete(fetchMembers())
 except KeyboardInterrupt:
     pass
 finally:
--- getactivity/settings.py
+++ getactivity/settings.py
@@ -1,6 +1,6 @@
 global key
-key = "&key=e650ec9899341f"
+key = "&key=8cd050cfc87d4c"
 global pnw
 pnw = "https://politicsandwar.com/api/"
 global disckey
-disckey = "ODM4NjAxNTE4OTAyNjczNDMw.YI9ehg.VaHeieueeTTx7q-DPHF9YifHkpA"
+disckey = "ODM4NjAxNTE4OTAyNjczNDMw.YI9ehg.gt_YFiGLpC0XRD2-dlVEsU2hf8s"
